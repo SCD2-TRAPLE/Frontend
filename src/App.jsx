620436276189513b0d87fd3cd7046d4e9ef5940f
배 <diana021015.gmail.com>
2024-05-08 17:14:07 +0900

    placeSelct setting

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+// @ts-nocheck
+import React from 'react';
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { SearchContentProvider } from './Context';
+import Main from 'src/Main';
+function App() {
+  return (
+    <>
+      <SearchContentProvider>
+        <div>
+          <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <Routes>
+              <Route path="/" element={<Main />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </SearchContentProvider>
+    </>
+  );
+}
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js', { type: 'text/javascript' })
+      .then((registration) => {
+        console.log('SW registered: ', registration);
+      })
+      .catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
+
+export default App;
---
+++ src/placeSelect.jsx
@@ -0,0 +1,3 @@
+//@ts-nocheck
+import React from "react";
+import { styles } from "src/components/placeSelectDetail/style";

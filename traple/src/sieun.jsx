78f13b13a2506fb64ad89b7433d13f6a39b5c9e9
sieun <leesieun030428@gmail.com>
2024-05-19 15:59:14 +0900

    firstcommit

--- traple/src/sieun.jsx
+++ traple/src/sieun.jsx
@@ -1,1 +1,15 @@
 import react from react;
+import Modal from 'react-modal';
+import style from './cchhaatt.css';
+
+function App(){
+    return (
+        <Modal 
+        isOpen = {true}
+        className = "modal-content"
+        overlayClassName = "modal-overlay"
+        >
+            모달입니다.
+        </Modal>
+    )
+}
